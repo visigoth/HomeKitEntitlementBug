bf68cd4a2637163b6866530b190502f4cf7a3e7e
Shaheen Gandhi <shaheen@fb.com>
2021-02-15 18:31:41 -0800

    Initial Commit

---
+++ HomeKitEntitlementBug/HomeKitEntitlementBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeKitEntitlementBugApp.swift
+//  HomeKitEntitlementBug
+//
+//  Created by Shaheen Gandhi on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeKitEntitlementBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
